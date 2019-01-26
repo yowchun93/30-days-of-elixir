d1f24ae22eb0a60276000171e5506facce03a37b
yc yap <yowchun93@gmail.com>
2019-01-26 21:33:31 +0800

    Recurse until list is empty

--- 04-elixir-lists-2.exs
+++ 04-elixir-lists-2.exs
@@ -37,6 +37,34 @@ defmodule MyList do
     max(tail, current_max)
   end
 
+
+  ## Complex List Patterns
+  ## [ 1,2,3] | [4,5,6] ] -> [1,2,3,4,5,6]
+  def swap([]) do
+    []
+  end
+
+  defp swap([a,b | tail]) do
+    [b,a | swap(tail)]
+  end
+
+  defp swap(_) do
+    raise "cant swap odd number of elements"
+  end
+
+  ## Recurse until list is empty stanza
+  def for_location_27([]) do
+    []
+  end
+
+  def for_location_27([ [time, 27, temp, rain] | tail ]) do
+    [ [time, 27, temp, rain] | for_location_27(tail) ]
+  end
+
+  def for_location_27([ _ | tail ]) do
+    for_location_27(tail)
+  end
+
 end
 
 defmodule ListTest do
@@ -48,6 +76,10 @@ defmodule ListTest do
 
   test "max in list" do
     MyList.max([1,2,3]) == 3
+  end
+
+  test "swap" do
+    MyList.swap([1,2,3]) == [2,1,3]
   end
 
 
