0f3a94601cb5110bdc942fb9ce8b4c8e2ef5f0d9
yc yap <yowchun93@gmail.com>
2019-02-01 07:51:19 +0800

    each and take implementations

--- 08-elixir-enum.ex
+++ 08-elixir-enum.ex
@@ -14,7 +14,6 @@ defmodule MyEnum do
     end
   end
 
-  ## not tail optimized
   def each([], _func) do
     :ok
   end
@@ -22,6 +21,33 @@ defmodule MyEnum do
   def each([head|tail], func) do
     func.(head)
     each(tail, func)
+    :ok
+  end
+
+  def filter([], func), do: []
+  def filter([head | tail], func) do
+    if func.(head) do
+      # elixir always returns a new list, instead of mutating the original list
+      [ head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+
+  def take(list, n) do
+    _take([], list, n)
+  end
+
+  def _take(result, [], _) do
+    []
+  end
+
+  def _take(result, [head|tail], n) do
+    _take([head|result], tail, n-1)
+  end
+
+  def _take(result, _, 0) do
+    result
   end
 
 end
@@ -38,7 +64,11 @@ defmodule EnumTest do
   end
 
   test "Enum each" do
-    MyEnum.each([2,3], fn(x) -> x*x end) == [4,9]
+    MyEnum.each([2,3], &IO.puts/1) == :ok
+  end
+
+  test "Enum filter" do
+    MyEnum.filter([2,3,6], fn(x) -> rem(x,3) == 0 end) == [3,6]
   end
 
 end
